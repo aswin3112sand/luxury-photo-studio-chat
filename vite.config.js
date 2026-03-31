6b70968468fe9baed0aed183e197f70ce22fc93e
Aswin <aswins3112sand@gmail.com>
2026-03-31 16:28:06 +0530

    git add -A

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   root: 'k-photo-studio',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
 })
